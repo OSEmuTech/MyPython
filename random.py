7a762aa697252fe8c47a7257f4d83d0e62767401
OSEmuTech <jwatter1@hotmail.com>
2016-03-31 01:13:30 -0400

    Added random.py which is a basic Dungeons and Dragons  stat roller.

---
+++ random.py
@@ -0,0 +1,17 @@
+import random
+import os
+
+
+t_abilities = ('Strength', 'Intelligence', 'Wisdom', \
+    'Dexterity', 'Constitution', 'Charisma')
+d = 'y'
+while d !='n':
+    os.system('clear')
+    for trait in t_abilities:
+        i_ra3 = 0
+        for i in range(1,4):
+            i_ra = random.randrange(1,7)
+            i_ra3 += i_ra
+            print('Roll #{} is {}.\tTotal: {}'.format(i,i_ra,i_ra3))
+        print('\t\t\t{}: {}\n'.format(trait,i_ra3))
+    d = str(input('Roll again? (y/n): '))
