2393502eb2b81473d4dcfa5139fc5fe0c1808ddc
a-men <jwatter1@hotmail.com>
2022-04-29 09:02:22 -0400

    change name of random.py due to recursion

--- random_rolls.py
+++ random_rolls.py
@@ -1,4 +1,4 @@
-import random
+import random as rn
 import os
 
 
@@ -10,7 +10,7 @@ while d !='n':
     for trait in t_abilities:
         i_ra3 = 0
         for i in range(1,4):
-            i_ra = random.randrange(1,7)
+            i_ra = rn.randrange(1,7)
             i_ra3 += i_ra
             print('Roll #{} is {}.\tTotal: {}'.format(i,i_ra,i_ra3))
         print('\t\t\t{}: {}\n'.format(trait,i_ra3))
