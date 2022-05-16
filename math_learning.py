f1eb3ab67f04aef1d1652fa6e530291eb51c8022
a-men <jwatter1@hotmail.com>
2022-05-16 02:26:04 -0400

    Changes

--- math_learning.py
+++ math_learning.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-a = np.arange(0,100000000)
+a = np.arange(0,10000000)
+
 start = time.time()
-a*a*a
+np.power(a,7)
 end = time.time()
+
 print(end-start)
